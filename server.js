49d953916537199f0e34b999e27d738cc4fb4b5e
Wesley <wesaboo@gmail.com>
2020-06-21 20:10:16 -0700

    fix MIME error, working on POST and DELETE

--- server.js
+++ server.js
@@ -12,7 +12,8 @@ const PORT = process.env.PORT || 3000;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'))  // https://expressjs.com/en/starter/static-files.html
+// Fixes MIME type error when attempting to load CSS 
+app.use(express.static('public'))
 
 // Routes
 // =============================================================
@@ -29,8 +30,33 @@ app.get("/notes", function (req, res) {
 
 // Basic route that sends the db.json file 
 app.get("/api/notes", function (req, res) {
-    res.sendFile(path.join(__dirname + "/db/", "db.json"));
+    res.sendFile(path.join(__dirname, "/db/db.json"));
 });
+
+// Create New Characters - takes in JSON input
+app.post("/api/tables", function (req, res) {
+    // req.body hosts is equal to the JSON post sent from the user
+    // This works because of our body parsing middleware
+    var newReservation = req.body;
+
+    // Using a RegEx Pattern to remove spaces from newCustomer
+    // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+    newReservation.routeName = newReservation.customerName.replace(/\s+/g, "").toLowerCase();
+
+    console.log(newReservation);
+    if (customers.length < 5) {
+        customers.push(newReservation)
+        newReservation.type = "Reservation"
+    }
+    else {
+        waitlist.push(newReservation);
+        newReservation.type = "Waitlist"
+    }
+    res.json(newReservation);
+
+});
+
+
 
 // Starts the server to begin listening
 // =============================================================
